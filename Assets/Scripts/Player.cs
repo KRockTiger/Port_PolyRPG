af81ba906cedde3b4edffae54944adb86d55836a
alsgh990710 <alsgh990710@naver.com>
2024-03-10 13:42:36 +0900

    03.10 - 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,12 @@ public class Player : MonoBehaviour
 {
     private CharacterController controller;
     private Animator animator;
-    [SerializeField] private float moveHorizontal; //x축 좌표 담당
-    [SerializeField] private float moveVertical; //z축 좌표 담당
+    private float moveHorizontal; //x축 좌표 담당
+    private float moveVertical; //z축 좌표 담당
 
     [Header("캐릭터 무빙 설정")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
     private float gravity = 9.81f;
 
     [Header("카메라 설정")]
@@ -32,8 +33,6 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Moving();
-        DirectionOfCamera();
-        AnimMoving();
     }
 
     /// <summary>
@@ -44,58 +43,72 @@ public class Player : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 inputMove = new Vector3(moveHorizontal, 0f, moveVertical);
-        Quaternion rotTurn = cam.transform.rotation;
-        rotTurn.y = 0f;
-        //controller.Move(inputMove * moveSpeed * Time.deltaTime);
+        Vector3 direction = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
+        //.normalized를 넣어서 대각선 이동이 빨리 가지 않기 위해 막아놓음
 
-        controller.Move(rotTurn * inputMove.normalized * Time.deltaTime * moveSpeed);
+        //float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Deg2Rad;
+        //transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        //controller.Move(direction * moveSpeed * Time.deltaTime);
+
+        //controller.Move(rotTurn * inputMove.normalized * Time.deltaTime * moveSpeed);
+
+        if (direction.magnitude >= 0.1f) //방향키를 입력할 때 적용할 수 있게 적용
+        //이 조건을 넣지 않으면 캐릭터가 안움직일 때 y회전 값이 0으로 고정
+        {
+            DirectionMovingOfCamera(direction);
+        }
+
+        AnimMoving(direction); //움직임에 따라 애니메이션 적용
     }
 
     /// <summary>
     /// 카메라의 위치를 이용해 캐릭터의 정방향을 정하게 하는 함수
     /// </summary>
-    private void DirectionOfCamera()
+    private void DirectionMovingOfCamera(Vector3 _direction)
     {
-        //Vector3 posCamera = cam.transform.position; //카메라의 위치를 저장
-        //posCamera.y = 0f; //y값은 방향에 관련 없으므로 0으로 처리
+        //내가 바라볼 방향을 각도로 전환
+        float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.transform.rotation.eulerAngles.y;
+        //※ Mathf.Deg2Rad를 넣으면 작동 안됨 ※
+        //cam.transform.rotation.eulerAngles.y를 넣어 카메라의 y회전값을 더해 카메라 방향을 정방향으로 설정
 
-        //Vector3 dir = (transform.forward - cam.transform.forward);
-        //camX = dir.x;
-        //camZ = dir.z;
-        //Vector3 direction = posCamera.normalized; //그대로 다시 저장 후 노멀라이즈
+        //현 캐릭터 방향에서 입력한 방향으로 회전할 때 부드럽게 움직일 수 있도록 설정
+        float smoothTurnY = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, targetAngle, ref turnSpeed, 0.1f);
 
-        //float angleY = Mathf.Atan2(dir.z, dir.x) * Mathf.Deg2Rad;
-        //Debug.Log($"dir = {dir},angleY = {angleY}");
-        //transform.eulerAngles = new Vector3 (0f, angle, 0f);
+        transform.rotation = Quaternion.Euler(0f, smoothTurnY, 0f); //설정한 값 적용시키기
+
+        controller.Move(_direction * moveSpeed * Time.deltaTime);
+
+        #region 회전 수정 전 코드
+        //transform.eulerAngles = new Vector3(0f, angle, 0f);
 
         //transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-        if (Input.GetKey(KeyCode.W))
-        { 
-            float targetAngle = cam.transform.rotation.eulerAngles.y;
-            float vel = 0f;
-            float fixedAngle = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, 
-                targetAngle, ref vel, 0.04f);
+        //if (Input.GetKey(KeyCode.W))
+        //{ 
+        //    float targetAngle = cam.transform.rotation.eulerAngles.y;
+        //    float vel = 0f;
+        //    float fixedAngle = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, 
+        //        targetAngle, ref vel, 0.04f);
 
-            //transform.rotation = Quaternion.Euler(new Vector3(0f, targetAngle, 0f));
-            transform.rotation = Quaternion.Euler(new Vector3(0f, fixedAngle, 0f));
-        }
+        //    //transform.rotation = Quaternion.Euler(new Vector3(0f, targetAngle, 0f));
+        //    transform.rotation = Quaternion.Euler(new Vector3(0f, fixedAngle, 0f));
+        //}
+        #endregion
     }
 
     /// <summary>
     /// 캐릭터의 움직임을 체크하여 애니메이션 적용
     /// </summary>
-    private void AnimMoving()
+    private void AnimMoving(Vector3 _moveCheck)
     {
-        Vector3 moveCheck = new Vector3(moveHorizontal, 0f, moveVertical); //위 입력값을 벡터로 넣기
+        //Vector3 moveCheck = _direction; //위 입력값을 벡터로 넣기
 
-        if (moveCheck.magnitude >= 0.1f) //움직임이 있으면 위 벡터의 길이 값이 0보다 크기 때문에 0.1보다 높게 측정
+        if (_moveCheck.magnitude >= 0.1f) //움직임이 있으면 위 벡터의 길이 값이 0보다 크기 때문에 0.1보다 높게 측정
         {
             animator.SetBool("Moving", true);
         }
 
-        else if (moveCheck.magnitude == 0f)
+        else if (_moveCheck.magnitude == 0f)
         {
             animator.SetBool("Moving", false);
         }
